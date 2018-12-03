f1f0ede053aa08ce942506cbc652b2c922875007
lauralee01 <lauraoghwono@gmail.com>
2018-12-03 22:06:29 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 import Users from './containers/Users';
 import User from './containers/User';
 import CreateUsers from './containers/CreateUsers';
 import Groups from './containers/Groups';
+import Group from './containers/Group';
 import CreateGroups from './containers/CreateGroups';
 import NotFound from './components/NotFound';
+import './styles.css';
+
 
   class App extends React.Component{
 
     render() {
       return (
         <BrowserRouter>
-          <div>
-            <ul>
+         <div id="container">
+          <aside>
+            <ul className="menu vertical-menu">
               <li><Link to="/">Home</Link></li>
-              <li>  
-                <Link to="/">Users</Link>
-                <ul>
-                  <li><Link to="/users/create">Create Users</Link></li>
-                </ul>
-              </li>
-              <li>
-                  <Link to="/groups">Groups</Link>
-                  <ul>
-                    <li><Link to="/groups/create">Create Groups</Link></li>
-                  </ul>
-              </li>
+              <li><Link to="/users">Users</Link></li>
+              <li><Link to="/users/create">Create Users</Link></li>
+              <li><Link to="/groups">Groups</Link></li>
+              <li><Link to="/groups/create">Create Groups</Link></li>
             </ul>
-
-            <hr />
+          </aside>
+           <div id="content">
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/users" component={Users} />
-              <Route path="/users/create" component={CreateUsers} />
-              <Route path="/users/:name" component={User} />
-              <Route exact path="/groups" component={Groups} />
-              <Route path="/groups/create" component={CreateGroups} />
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/users" component={Users}/>
+              <Route path="/users/create" component={CreateUsers}/>
+              <Route path="/users/:name" component={User}/>
+              <Route exact path="/groups" component={Groups}/>
+              <Route path="/groups/create" component={CreateGroups}/>
               <Route path="*" component={NotFound}/>
             </Switch>
+           </div>
            </div>
         </BrowserRouter>
       )
